cc61579e627535b3e88588c6e6dd32118ab987ac
Gustavo Adolfo Bernal Acero <gustavober98@gmail.com>
2026-01-15 16:08:17 -0500

    feat: added view login

--- src/app/features/auth/login-component/login-component.ts
+++ src/app/features/auth/login-component/login-component.ts
@@ -3,9 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-login-component',
   imports: [],
-  templateUrl: "./login-component.html",
+  templateUrl: './login-component.html',
   styleUrl: './login-component.scss',
 })
 export class LoginComponent {
+  estado: boolean = false;
 
+  togglePassword() {
+    this.estado = !this.estado;
+  }
 }
